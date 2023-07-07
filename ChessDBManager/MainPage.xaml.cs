9a66f528c1533f3cf9da0feba3cae21466c6f5f6
antonifritz <256763@student.pwr.edu.pl>
2023-07-07 13:02:46 +0200

    Dodaj pliki projektów.

---
+++ ChessDBManager/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System.Text.RegularExpressions;
+
+namespace ChessDBManager;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void ImportFileAsync(object sender, EventArgs e)
+    {
+        var options = new PickOptions
+        {
+            PickerTitle = "Please select a PGN file",
+            FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.WinUI, new string[] { ".pgn" } } 
+        })
+        };
+
+        var result = await FilePicker.PickAsync(options);
+
+        if (result != null)
+        {
+            using var streamReader = new StreamReader(await result.OpenReadAsync());
+            string line;
+            while ((line = await streamReader.ReadLineAsync()) != null)
+            {
+                Match headerMatch = Regex.Match(line, @"\[([A-Za-z]+)\s+""([^""]+)""\]");
+                if (headerMatch.Success)
+                {
+                    //HeaderDictionary[headerMatch.Groups[1].Value] = headerMatch.Groups[2].Value;
+                    continue;
+                }
+            }
+
+        }
+    }
+}
+
---
+++ ChessDBManager/PgnFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDBManager
+{
+    public class PgnFile
+    {
+        private Dictionary<string, string> HeaderDictionary { get; set; }
+
+    }
+}
